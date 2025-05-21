36adcabdea91a812fd1b5f04ce6947e09a989564
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-05-21 10:41:43 -0300

    feat: implement Register page layout with form and navigation

--- src/ui/pages/Register/index.tsx
+++ src/ui/pages/Register/index.tsx
@@ -1,3 +1,34 @@
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
 export function Register() {
-  return <div>Ca esta o register</div>;
+	return (
+		<div>
+			<header className="flex flex-col items-center gap-4">
+				<h1 className="text-2xl font-bold text-gray-900 tracking-[-0.5px]">
+					Crie sua conta
+				</h1>
+				<p className="space-x-2">
+					<span className="text-gray-700 tracking-[-0.5px]">
+						Já possui uma conta?
+					</span>
+					<Link
+						to="/login"
+						className="tracking-[-0.5px] font-medium text-teal-900"
+					>
+						Fazer Login
+					</Link>
+				</p>
+			</header>
+
+			<form className="flex flex-col mt-[60px] gap-4">
+				<Input type="text" placeholder="Nome" name="name" />
+				<Input type="email" placeholder="Email" name="email" />
+				<Input type="password" placeholder="Senha" name="password" />
+
+				<Button>Criar a conta</Button>
+			</form>
+		</div>
+	);
 }
